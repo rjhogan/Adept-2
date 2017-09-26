fcd8a9887faeebc9281402aa713911537c0ba16f
Robin Hogan <r.j.hogan@ecmwf.int>
2017-09-26 22:25:06 +0100

    Increased version to 1.9.11

--- include/adept/base.h
+++ include/adept/base.h
@@ -22,8 +22,8 @@
 
 // The version of the Adept library is specified both as a string and
 // an integer
-#define ADEPT_VERSION      10910
-#define ADEPT_VERSION_STR "1.9.10"
+#define ADEPT_VERSION      10911
+#define ADEPT_VERSION_STR "1.9.11"
 
 
 // ---------------------------------------------------------------------
