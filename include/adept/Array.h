3561ccd67d62e0e872451a394e8a2c6d7850e0bb
Robin Hogan <r.j.hogan@reading.ac.uk>
2020-07-21 08:40:24 +0100

    Moved GradientIndex to its own header

--- include/adept/Array.h
+++ include/adept/Array.h
@@ -35,6 +35,7 @@
 #include <adept/IndexedArray.h>
 #include <adept/where.h>
 #include <adept/noalias.h>
+#include <adept/GradientIndex.h>
 
 namespace adept {
   using namespace adept::internal;
@@ -119,59 +120,6 @@ namespace adept {
     struct array_helper<2,false>   { const char* name() { return "Matrix"; } };
     template <>
     struct array_helper<2,true>    { const char* name() { return "aMatrix"; } };
-
-    // Arrays inherit from this class to provide optional storage of
-    // the gradient index of the first value of the array depending on
-    // whether the array is active or not
-    template <bool IsActive>
-    struct GradientIndex {
-      // Constructor used when linking to existing data where gradient
-      // index is known
-      GradientIndex(Index val = -9999) : value_(val) { }
-      // Constructor used for fixed array objects where length is
-      // known
-      GradientIndex(Index n, bool) : value_(ADEPT_ACTIVE_STACK->register_gradients(n)) { }
-      GradientIndex(Index val, Index offset) : value_(val+offset) { }
-      Index get() const { return value_; }
-      void set(Index val) { value_ = val; }
-      void clear() { value_ = -9999; }
-      template <typename Type>
-      void set(const Type* data, const Storage<Type>* storage) {
-	value_ = (storage->gradient_index() + (data - storage->data()));
-      }
-      void assert_inactive() {
-	throw invalid_operation("Operation applied that is invalid with active arrays"
-				ADEPT_EXCEPTION_LOCATION);
-      }
-      void unregister(Index n) { ADEPT_ACTIVE_STACK->unregister_gradients(value_, n); }
-#ifdef ADEPT_MOVE_SEMANTICS
-      void swap_value(GradientIndex& rhs) noexcept {
-	Index tmp_value = rhs.get();
-	rhs.set(value_);
-	value_ = tmp_value;
-      }
-#endif
-    private:
-      Index value_;
-    };
-
-    template <>
-    struct GradientIndex<false> {
-      GradientIndex(Index val = -9999) { }
-      GradientIndex(Index, bool) { }
-      GradientIndex(Index val, Index offset) { }
-      Index get() const { return -9999; }
-      void set(Index val) { }
-      void clear() { }
-      template <typename Type>
-      void set(const Type* data, const Storage<Type>* storage) { }
-      void assert_inactive() { }
-      void unregister(Index) { }
-#ifdef ADEPT_MOVE_SEMANTICS
-      void swap_value(GradientIndex& rhs) noexcept { }
-#endif
-    };
-
 
   } // End namespace internal
 
