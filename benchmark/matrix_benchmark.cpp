dd32a415bebba998c581395251cc8055dea8db15
Robin Hogan <r.j.hogan.ecmwf.int>
2023-03-29 21:49:28 +0200

    Added timing of solve to matrix_benchmark

--- benchmark/matrix_benchmark.cpp
+++ benchmark/matrix_benchmark.cpp
@@ -6,7 +6,7 @@
 
 template<bool IsActive>
 double
-time_operation(int n, int nrepeat, bool is_col_major)
+time_matmul(int n, int nrepeat, bool is_col_major)
 {
   adept::Array<2,double,IsActive> A, B, C;
   Timer timer;
@@ -24,6 +24,8 @@ time_operation(int n, int nrepeat, bool is_col_major)
   for (int irepeat = -nrepeat/10; irepeat < nrepeat; ++irepeat) {
     A = 1.1;
     B = 2.2;
+    A.diag_vector() = 3.3;
+    B.diag_vector() = 5.5;
     if (IsActive) {
       adept::active_stack()->new_recording();
     }
@@ -35,12 +37,47 @@ time_operation(int n, int nrepeat, bool is_col_major)
       timer.stop();
     }
   }
+  /*
   if (IsActive && n < 8) {
     std::cout << "C=" << C;
     std::cout << *adept::active_stack();
     adept::active_stack()->print_statements();
   }
+  */
   return timer.timing(matmul_timer_id) / nrepeat;
+}
+
+double
+time_solve(int n, int nrepeat, bool is_col_major)
+{
+  adept::Matrix A, B, C;
+  Timer timer;
+  int solve_timer_id = timer.new_activity("solve");
+  if (is_col_major) {
+    A.resize_column_major(adept::expression_size(n,n));
+    B.resize_column_major(adept::expression_size(n,n));
+    C.resize_column_major(adept::expression_size(n,n));
+  }
+  else {
+    A.resize(n,n);
+    B.resize(n,n);
+    C.resize(n,n);
+  }
+  for (int irepeat = -nrepeat/10; irepeat < nrepeat; ++irepeat) {
+    A = 1.1;
+    B = 2.2;
+    A.diag_vector() = 3.3;
+    B.diag_vector() = 5.5;
+    if (irepeat >= 0) {
+      timer.start(solve_timer_id);
+    }
+    C = adept::solve(A, B);
+    if (irepeat >= 0) {
+      timer.stop();
+    }
+  }
+
+  return timer.timing(solve_timer_id) / nrepeat;
 }
 
 
@@ -48,24 +85,41 @@ int
 main(int argc, char* argv[])
 {
   int ibegin = 1;
-  int iend = 18;
-  int nrepeat = 10;
+  int iend = 8;
+  int nrepeat = 20;
   bool is_col_major = false;
 
   adept::Stack stack;
   int n = 2;
+  std::cout << "Average cost per operation (" << nrepeat << " repeats)\n";
   std::cout << "Dense N-by-N matrix-matrix multiplication\n";
-  std::cout << " N        inactive time (us)   inactive flops    active time (us)    active flops\n";
+  //std::cout << " N        inactive time (us)   inactive flops    active time (us)    active flops\n";
+  std::cout << "N \tinactive time (us) \tactive time (us)\n";
   for (int i = ibegin; i <= iend; ++i) {
-    std::cout << n << "  ";
+    std::cout << n << " \t";
 
-    double t = time_operation<false>(n, nrepeat, is_col_major);
-    std::cout << t*1.0e6 << "  " << (n*n*n) / t << "  ";
+    double t = time_matmul<false>(n, nrepeat, is_col_major);
+    //    std::cout << t*1.0e6 << "  " << (n*n*n) / t << "  ";
+    std::cout << t*1.0e6/nrepeat << " \t\t\t";
 
-    t = time_operation<true>(n, nrepeat, is_col_major);
-    std::cout << t*1.0e6 << "  " << (n*n*n) / t;
+    t = time_matmul<true>(n, nrepeat, is_col_major);
+    //    std::cout << t*1.0e6 << "  " << (n*n*n) / t;
+    std::cout << t*1.0e6/nrepeat;
 
     std::cout << "\n";
+
+    n *= 2;
+  }
+  
+  n = 2;
+  std::cout << "Dense N-by-N matrix-matrix solve\n";
+  std::cout << "N \tinactive time (us)\n";
+  for (int i = ibegin; i <= iend; ++i) {
+    std::cout << n << " \t";
+
+    double t = time_solve(n, nrepeat, is_col_major);
+    //    std::cout << t*1.0e6 << "  " << (n*n*n) / t << "  ";
+    std::cout << t*1.0e6/nrepeat << "\n";
 
     n *= 2;
   }
